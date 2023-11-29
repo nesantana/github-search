f82551f494175643b75cbc6d0283e4120811468c
Mateus Santana <nesantana15@gmail.com>
2023-11-29 11:51:04 -0400

    finish first page with logic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,58 @@
 'use client'
 
 import { FaSearch } from 'react-icons/fa'
+import { useMemo, useState } from 'react'
+import { isEmpty } from 'lodash'
+import { useRouter } from 'next/navigation'
 import { Form } from './styled'
+import { useToast } from '@/hooks/useToast'
 
-const Home = () => (
-  <main>
-    <div className="container text-center">
-      <Form>
-        <img src="/assets/logo.png" alt="Logo GitHub Search" />
+const Home = () => {
+  const [term, setTerm] = useState<string>('')
 
-        <div className="position-relative">
-          <input type="text" placeholder="Pesquise o nome de usuário que deseja buscar..." />
+  const router = useRouter()
+  const { showError } = useToast()
 
-          <FaSearch />
-        </div>
-      </Form>
-    </div>
-  </main>
-)
+  const pageToGo = useMemo(() => `/search/${term}/`, [term])
+
+  const goToPage = () => {
+    if (isEmpty(term)) {
+      showError('Por favor, preencha o nome de usuário!')
+
+      return
+    }
+
+    router.push(pageToGo)
+  }
+
+  const handleSearch = (e: any) => {
+    if (e.key === 'Enter') {
+      goToPage()
+    }
+  }
+
+  return (
+    <main>
+      <div className="container text-center">
+        <Form>
+          <img src="/assets/logo.png" alt="Logo GitHub Search" />
+
+          <div className="position-relative">
+            <input
+              type="text"
+              placeholder="Pesquise o nome de usuário que deseja buscar..."
+              value={term}
+              onChange={({ target }) => setTerm(target.value)}
+              onKeyDown={handleSearch}
+
+            />
+
+            <FaSearch onClick={goToPage} />
+          </div>
+        </Form>
+      </div>
+    </main>
+  )
+}
 
 export default Home
